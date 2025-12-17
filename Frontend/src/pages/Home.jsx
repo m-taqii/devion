cc9384213cca24b647d4f3dc0ddd16f57c6d669e
m-taqii <busy.digital.master@gmail.com>
2025-12-17 15:29:19 +0500

    Setting Canvas as Background

--- Frontend/src/pages/Home.jsx
+++ Frontend/src/pages/Home.jsx
@@ -21,45 +21,65 @@ const Home = () => {
   const [ismobile, setIsmobile] = useState(false)
 
   useEffect(() => {
-    if(window.innerWidth < 768 ){
+    if (window.innerWidth < 768) {
       setIsmobile(true)
     }
   }, [])
-  
 
-useGSAP(() => {
-  gsap.registerPlugin(ScrollTrigger)
- 
-})
+
+  useGSAP(() => {
+    gsap.registerPlugin(ScrollTrigger)
+
+  })
   return (
     <div className='min-h-screen w-full bg-black'>
-      <Navbar />
+      <div className="relative z-50 pointer-events-auto">
+        <Navbar />
+      </div>
+
+
+      <div className='fixed inset-0 z-0 w-full h-full bg-black'>
+
+        {/* Background Effect*/}
+        <div className="absolute top-1/2 left-1/2 transform -translate-x-1/2 -translate-y-1/2 w-[500px] h-[500px] bg-indigo-500/20 rounded-full blur-[100px] animate-pulse"></div>
+        <div className="absolute top-1/2 left-1/2 transform -translate-x-1/2 -translate-y-1/2 w-[300px] h-[300px] bg-blue-600/30 rounded-full blur-[80px]"></div>
+
+        {/* Noise overlay */}
+        <div className="absolute inset-0 bg-[url('/noise.svg')] opacity-20"></div>
+        {/* Canvas for 3D scene */}
+        <CanvasExperience ismobile={ismobile} />
+      </div>
 
       {/* Hero Section */}
-  
-        <div className='fixed inset-0 z-0 pointer-events-none w-full h-full bg-black'>
+      <section className='relative z-10 pointer-events-none h-screen w-full'>
+        <img
+          src="/devion.png"
+          alt="Devion Logo"
+          className='pointer-events-auto absolute w-170 z-51 top-70 md:top-55 left-1/2 transform -translate-x-1/2 mix-blend-difference'
+          fetchPriority="high"
+          loading="eager"
+        />
+      </section>
 
-          {/* Background */}
-          <div className="absolute top-1/2 left-1/2 transform -translate-x-1/2 -translate-y-1/2 w-[500px] h-[500px] bg-indigo-500/20 rounded-full blur-[100px] animate-pulse"></div>
-          <div className="absolute top-1/2 left-1/2 transform -translate-x-1/2 -translate-y-1/2 w-[300px] h-[300px] bg-blue-600/30 rounded-full blur-[80px]"></div>
-
-          {/* Noise overlay */}
-          <div className="absolute inset-0 bg-[url('/noise.svg')] opacity-20"></div>
-          <img
-            src="/devion.png"
-            alt="Devion Logo"
-            className='absolute w-170 z-51 top-70 md:top-55 left-1/2 transform -translate-x-1/2 mix-blend-difference'
-            fetchPriority="high"
-            loading="eager"
-          />
-          {/* Canvas for 3D scene */}
-          <CanvasExperience ismobile={ismobile}/>
+      {/* Capabilities Section */}
+      <section className='relative z-10 h-screen w-full'>
+        <div className="flex flex-col gap-3 h-screen m-5">
+          <h2 className="pointer-events-auto text-4xl font-bold text-blue-500 text-right">Capabilities</h2>
+          {items.map((item) => {
+            return (
+              <ul className='flex justify-end gap-2' key={item}>
+                <li className="pointer-events-auto text-white text-md border border-blue-500 p-1 rounded-lg">{item}</li>
+              </ul>
+            )
+          })}
         </div>
+      </section>
 
       {/* Footer */}
-      <Footer />
+      <div className="relative z-50 pointer-events-auto">
+        <Footer />
+      </div>
     </div>
   )
 }
-
 export default Home
