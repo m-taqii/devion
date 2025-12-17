ae3647c55a44a0ae33983d2a7984aa8f4a437256
m-taqii <busy.digital.master@gmail.com>
2025-12-17 18:51:43 +0500

    updating server config

--- Backend/server.js
+++ Backend/server.js
@@ -1,10 +1,26 @@
 require('dotenv').config();
 const app = require('./src/app');
-const port = process.env.PORT || 3000;
 const connectDB = require('./src/db/db');
-connectDB();
 
+const port = process.env.PORT || 3000;
 
-app.listen(port,'0.0.0.0', () => {
-  console.log(`Server is running on port ${port}`);
+app.get('/', (req, res) => {
+  res.send('API is running successfully');
 });
+
+const startServer = async () => {
+  try {
+    await connectDB();
+    console.log("MongoDB connected");
+
+    app.listen(port, '0.0.0.0', () => {
+      console.log(`Server is running on port ${port}`);
+    });
+    
+  } catch (error) {
+    console.error("Failed to start server:", error);
+    process.exit(1); 
+  }
+};
+
+startServer();
